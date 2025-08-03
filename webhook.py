9adb705e9dcdec49d784076cff4cbb9f91a2b630
Cosme Alves <cosme.alex@gmail.com>
2025-08-03 19:56:43 +0000

    Fix: method post

--- webhook.py
+++ webhook.py
@@ -2,25 +2,22 @@ import os
 import hmac
 import hashlib
 import json
-from datetime import datetime
-from flask import Flask, request, abort, jsonify
+from datetime import datetime, timezone
+from flask import Flask, request, abort, jsonify, send_from_directory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 GITHUB_SECRET = os.getenv("GITHUB_SECRET", "undefined-secret")
-
 LOG_FILE = 'webhook.log'
 
-
 def log_event(event_type, status, message):
-    now = datetime.utcnow().isoformat()
+    now = datetime.now(timezone.utc).isoformat()
     log_line = f"[{now} - {event_type.upper()} - {status.upper()}] {message}"
     print(log_line)
     with open(LOG_FILE, 'a') as f:
         f.write(log_line + "\n")
-
 
 def verify_signature(data, signature_header):
     if not signature_header:
@@ -29,6 +26,9 @@ def verify_signature(data, signature_header):
     expected = f"sha1={mac.hexdigest()}"
     return hmac.compare_digest(expected, signature_header)
 
+@app.route('/', methods=['GET'])
+def index():
+    return send_from_directory('.', 'index.html')
 
 @app.route('/webhook', methods=['POST'])
 def github_webhook():
@@ -42,17 +42,11 @@ def github_webhook():
 
     try:
         json_data = request.get_json()
-        log_event(event, '200', f"Push recebido em: {datetime.utcnow()} - Repo: {json_data['repository']['full_name']}")
+        log_event(event, '200', f"Push recebido em: {datetime.now(timezone.utc)} - Repo: {json_data['repository']['full_name']}")
         return jsonify({'status': 'ok'}), 200
     except Exception as e:
         log_event(event, '500', f"Erro ao processar JSON: {e}")
         abort(500)
-
-
-@app.route('/', methods=['GET'])
-def index():
-    return "Webhook ativo."
-
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
